6855991eb7aa0e4d22907fc97cfd0fd53bbba4b1
ritesh salve <riteshsalve07@gmail.com>
2024-03-13 18:54:04 +0530

    finaldone

--- my-app/src/pages/Homepage.jsx
+++ my-app/src/pages/Homepage.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getCards } from "../redux/action";
+import { ImCross } from "react-icons/im";
+import moment from 'moment';
+import { GETCARDS_SUCCESS } from "../redux/actionType";
 
 import img1 from "../Images/Ellipse 1.png";
 import img2 from "../Images/Rectangle 17.png";
 import img3 from "../Images/Rectangle 31.png";
-import { ImCross } from "react-icons/im";
-import moment from 'moment';
-function Homepage({ togle2, togle,pageLast,page }) {
-// const [page,setPage]=useState(6)
-// const [pagelast,setPageLast]=useState(0)
+
+function Homepage({ togle2, togle, pageLast, page }) {
   const [isOpen, setIsOpen] = useState(false);
-console.log(pageLast,page)
+  const [cards, setCards] = useState([]);
+  
   const openModal = () => {
     setIsOpen(true);
   };
@@ -20,30 +21,38 @@ console.log(pageLast,page)
     setIsOpen(false);
   };
 
-  const carddata = useSelector((store) => store);
-  console.log(carddata?.carddata);
+  const carddata = useSelector((store)=>store);
+  const Data = carddata?.carddata;
+
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getCards());
   }, []);
 
-  const date=Date.now()
+  const date = Date.now();
   let formattedDate = moment(date).format('MMMM Do YYYY, h:mm:ss a');
-console.log(formattedDate); // e.g., "April 6th 2022, 2:30:00 pm"
+
+  const removeCard = (id) => {
+    const updatedCards = cards.filter(card => card.id !== id);
+    setCards(updatedCards);
+  };
+
+  useEffect(() => {
+    if (Data) {
+      setCards(Data);
+    }
+  }, [Data]);
+
   return (
     <>
-
-{isOpen && (
-        <div
-          id="default-modal"
-          tabIndex="-1"
-          aria-hidden="true"
-          className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-10  justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full m-auto"
-        >
+      {/* Modal Code */}
+      {isOpen && (
+        <div id="default-modal" tabIndex="-1" aria-hidden="true" className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-10 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full m-auto">
           <div className="relative p-4 w-full max-w-2xl max-h-full">
             <div className="relative bg-white rounded-lg shadow ">
               <div className="flex items-center justify-between p-4 md:p-5 ">
-               <img className="w-full" src={img3} alt="" />
+                <img className="w-full" src={img3} alt="" />
                 <button
                   onClick={closeModal}
                   type="button"
@@ -67,95 +76,59 @@ console.log(formattedDate); // e.g., "April 6th 2022, 2:30:00 pm"
                   <span className="sr-only">Close modal</span>
                 </button>
               </div>
-              
-              
             </div>
           </div>
         </div>
       )}
 
-
-
-
-
-
-
-
-
-
-      {carddata?.carddata.map((e,index) => {
-        if(index>pageLast&&index<page){
-          return(
-          
-            <div key={e.id} className="m-auto  flex justify-center mt-12 ">
-            {togle ? (
-              <div className="flex gap-2 ">
-    
-              <div class="flex flex-col items-center bg-white  rounded-lg  md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-white-800 
-              shadow-lg shadow-indigo-100/40 
-              " 
-              onClick={openModal}>
-                <img
-                  class=" w-20 rounded-t-lg  md:rounded-none md:rounded-s-lg"
-                  src={img1}
-                  alt=""
-                />
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-black-900 dark:text-black">{`${e.title.substr(
-                    0,
-                    20
-                  )}...`}</h5>
-                  <p class="mb-3 font-normal text-black-700 dark:text-black-400">{`${e.body.substr(
-                    0,
-                    50
-                  )}...`}</p>
-                  <h3 className="text-midGray font-bold">{formattedDate}</h3>
-                </div>
-                <div className="flex justify-center  items-center ml-2 rounded-xl">
-                 
-                </div>
-                
-              </div>
-              <div className="rounded-xl text-pinkii flex justify-center items-center">
-                    <ImCross />
+      {/* Display Cards */}
+      {cards.map((card, index) => {
+        if (index > pageLast && index < page) {
+          return (
+            <div key={card.id} className="m-auto  flex justify-center mt-12 ">
+              {togle ? (
+                <div className="flex gap-2 ">
+                  <div class="flex flex-col items-center bg-white  rounded-lg  md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-white-800 shadow-lg shadow-indigo-100/40 " onClick={openModal}>
+                    <img class=" w-20 rounded-t-lg  md:rounded-none md:rounded-s-lg" src={img1} alt="" />
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-black-900 dark:text-black">{`${card.title.substr(0, 20)}...`}</h5>
+                      <p class="mb-3 font-normal text-black-700 dark:text-black-400">{`${card.body.substr(0, 50)}...`}</p>
+                      <h3 className="text-midGray font-bold">{formattedDate}</h3>
+                    </div>
+                    <div className="flex justify-center  items-center ml-2 rounded-xl">
+                    </div>
                   </div>
-              </div>
-            ) : null}
-    
-            {togle2 ? (
-              <div className="">
-                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:border-gray-700">
-                  <div class="p-5 ">
-                    <a href="#">
-                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-black-900 dark:text-black">{`${e.title.substr(
-                        0,
-                        20
-                      )}...`}</h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{`${e.body.substr(
-                      0,
-                      50
-                    )}...`}</p>
-                    <h3 className="text-midGray font-bold">{formattedDate}</h3>
-                    <div className="m-auto flex justify-center">
-                      <img class="w-full" src={img2} alt="" />
+                  <div className="rounded-xl text-pinkii flex justify-center items-center">
+                    <ImCross onClick={() => removeCard(card.id)} />
+                  </div>
+                </div>
+              ) : null}
+
+              {/* Display togle2 content */}
+              {togle2 ? (
+                <div className="">
+                  <div class="max-w-sm bg-white border  rounded-lg shadow-xl ">
+                    <div class="p-5 ">
+                      <a href="#" className="flex justify-between">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black-900 dark:text-black">{`${card.title.substr(0, 20)}...`}</h5>
+                        <div className="rounded-xl text-pinkii flex justify-center items-center">
+                    <ImCross onClick={() => removeCard(card.id)} />
+                  </div>
+                      </a>
+                      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{`${card.body.substr(0, 50)}...`}
+                      </p>
+                      <h3 className="text-midGray font-bold">{formattedDate}</h3>
+                      <div className="m-auto flex justify-center">
+                        <img class="w-full" src={img2} alt="" />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ) : null}
-    
-    
-            
-          </div>
-            )
+              ) : null}
+            </div>
+          )
         }
-       
-      }
-       
-      )}
-<br />
-
+      })}
     </>
   );
 }
--- my-app/src/redux/action.js
+++ my-app/src/redux/action.js
@@ -5,6 +5,11 @@ import { GETCARDS_FAILURE, GETCARDS_REQUEST, GETCARDS_SUCCESS } from "./actionTy
 export const getCards=()=>(dispatch)=>{
     dispatch({type:GETCARDS_REQUEST})
     axios.get(`https://jsonplaceholder.typicode.com/posts?page=1&&limit=10`)
-    .then((res)=>dispatch({type:GETCARDS_SUCCESS,payload:res.data}))
+    .then((res)=>{
+        console.log(res.data)
+        return(
+            dispatch({type:GETCARDS_SUCCESS,payload:res.data}))}
+        )
+       
     .catch(()=>dispatch({type:GETCARDS_FAILURE}))
 }
